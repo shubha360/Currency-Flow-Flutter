29fbe7cdb28a4a3317e84f2e7a1da98a13ba65db
Raquibul Islam <shubha360@outlook.com>
2020-07-04 13:21:48 +0600

    Fixed orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/services.dart';
+
 import 'Screens/Conversion_Screen.dart';
 import 'package:flutter/material.dart';
 
@@ -8,6 +10,10 @@ void main() {
 class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       home: ConversionScreen(),
     );
